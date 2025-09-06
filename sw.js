796c27f48abcb85bd45d6c338e99dca6665d3fdc
k1h1tdk1 <nbmjdisk@gmail.com>
2025-09-06 21:11:25 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = "jinro-memo-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e=>{
+  const {request} = e;
+  if(request.method !== "GET") return;
+  e.respondWith(
+    caches.match(request).then(cached=>{
+      return cached || fetch(request).then(res=>{
+        const copy = res.clone();
+        caches.open(CACHE).then(c=>c.put(request, copy));
+        return res;
+      }).catch(()=>cached);
+    })
+  );
+});
